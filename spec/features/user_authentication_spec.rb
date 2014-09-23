533a3115d3ab5182d6b92b120e0457bda5c8eb60
Dan Croak <dan@thoughtbot.com>
2014-09-23 16:35:00 -0700

    Fix client/server-side analytics race condition
    We are seeing bad analytics data in Mixpanel because
    we are not aliasing guests into users.
    
    https://mixpanel.com/docs/integration-libraries/using-mixpanel-alias
    
    Segment.io strongly recommends doing this client-side:
    
    https://segment.io/docs/integrations/mixpanel/#alias
    
    Restrict user-facing flash messages to a known whitelist
    so we don't accidentally send `flash[:signed_up]` to the view.
    
    https://trello.com/c/ZzJdRvJu

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -11,42 +11,15 @@ feature 'User authentication' do
     expect(analytics).to have_tracked("Signed In").for_user(user)
   end
 
-  context "signs up" do
-    scenario "when user doesn't exist" do
-      github_username = "croaky"
-      user = build(:user, github_username: github_username)
-      stub_repo_requests(AuthenticationHelper::GITHUB_TOKEN)
+  scenario "when user doesn't exist, signs up" do
+    github_username = "croaky"
+    user = build(:user, github_username: github_username)
+    stub_repo_requests(AuthenticationHelper::GITHUB_TOKEN)
 
-      sign_in_as(user)
+    sign_in_as(user)
 
-      user = User.where(github_username: github_username).first
-      expect(page).to have_content(github_username)
-      expect(analytics).to have_tracked("Signed Up").for_user(user)
-    end
-
-    scenario "with campaign params" do
-      github_username = "croaky"
-      user = build(:user, github_username: github_username)
-      stub_repo_requests(AuthenticationHelper::GITHUB_TOKEN)
-      campaign_params = {
-        utm_campaign: "adwords-ruby",
-        utm_medium: "paidsearch",
-        utm_source: "adwords",
-      }
-      campaign_context = {
-        name: "adwords-ruby",
-        medium: "paidsearch",
-        source: "adwords",
-      }
-
-      sign_in_as(user, campaign_params)
-
-      user = User.where(github_username: github_username).first
-      expect(page).to have_content(github_username)
-      expect(analytics).to have_tracked("Signed Up").
-        for_user(user).
-        with(context: { campaign: campaign_context })
-    end
+    user = User.where(github_username: github_username).first
+    expect(page).to have_content(github_username)
   end
 
   scenario 'user signs out' do
